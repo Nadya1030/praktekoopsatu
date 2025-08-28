802c69451d6c367002678ef88044e0ad059f0e23
Nadya1030 <h1101241030@student.untan.ac.id>
2025-08-28 13:47:10 +0700

    Create home.php

---
+++ home.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Contoh PHP Sederhana</title>
+</head>
+<body>
+    <?php
+        echo "Halo, dunia!";
+
+    ?>
+  <a href="index.php">Go to Index Page</a>
+
+</body>
+</html>
